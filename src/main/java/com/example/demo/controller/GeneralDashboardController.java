0d567f3136c07a5e0693021b23dad85d9036267e
Lir3 <sinkuro0805@gmail.com>
2025-01-23 12:32:28 +0900

    111

--- src/main/java/com/example/demo/controller/GeneralDashboardController.java
+++ src/main/java/com/example/demo/controller/GeneralDashboardController.java
@@ -11,8 +11,8 @@ public class GeneralDashboardController {
 
 	@GetMapping("/generaldashboard")
 	public String generalDashboard(HttpSession session, Model model) {
-		String username = (String) session.getAttribute("username"); // セッションから username を取得
-		model.addAttribute("username", username);
+		String userid = (String) session.getAttribute("userid"); // セッションから userid を取得
+		model.addAttribute("userid", userid);
 		return "generaldashboard";
 	}
 }
