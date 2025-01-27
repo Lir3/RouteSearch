bb45a0033f7e61ab00bfb4d680a058f52d5dcfd3
Lir3 <sinkuro0805@gmail.com>
2025-01-27 11:57:17 +0900

    useridからusernameに変更

--- src/main/java/com/example/demo/controller/GeneralDashboardController.java
+++ src/main/java/com/example/demo/controller/GeneralDashboardController.java
@@ -11,8 +11,8 @@ public class GeneralDashboardController {
 
 	@GetMapping("/generaldashboard")
 	public String generalDashboard(HttpSession session, Model model) {
-		String userid = (String) session.getAttribute("userid"); // セッションから userid を取得
-		model.addAttribute("userid", userid);
+		String username = (String) session.getAttribute("username"); // セッションから username を取得
+		model.addAttribute("username", username);
 		return "generaldashboard";
 	}
 }
